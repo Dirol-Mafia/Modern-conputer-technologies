866ca7defb26494d0f9befd0e68373e3153e83df
Diana <dianisia.m@yandex.ru>
2017-01-10 18:06:07 +0300

    Add printing and editing images, founded by tags

--- Test/imageswithtags.h
+++ Test/imageswithtags.h
@@ -6,6 +6,13 @@
 #include <QString>
 #include <QStandardItem>
 #include <QStandardItemModel>
+#include <QPrinter>
+#include <QPrintDialog>
+#include <QPrintPreviewDialog>
+#include <QPainter>
+#include <QDebug>
+#include <QVector>
+#include <imageeditform.h>
 
 namespace Ui {
 class ImagesWithTags;
@@ -19,8 +26,19 @@ public:
     explicit ImagesWithTags(QStringList paths, QWidget *parent = 0);
     ~ImagesWithTags();
 
+private slots:
+    void on_printButton_clicked();
+    void drawImagesOnSheet(QPrinter* printer);
+    void on_editButton_clicked();
+
+    void on_tableView_clicked(const QModelIndex &index);
+
 private:
+    void callPrinter();
+    void getSelectedItems();
     Ui::ImagesWithTags *ui;
+    QStringList paths;
+    QVector <int> selectedItems;
 };
 
 #endif // IMAGESWITHTAGS_H
